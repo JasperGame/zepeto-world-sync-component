e7cff2668b5cc33eb5003d68de8ac6b159e3ff35
Kim JeongUng <123578202+JasperGame@users.noreply.github.com>
2023-03-29 10:31:50 +0900

    update module dependency

--- Assets/ZepetoScripts/MultiplaySync/Sample Code/RunningGame/ReportItems.ts
+++
@@ -1,9 +0,0 @@
-import { ZepetoScriptBehaviour } from 'ZEPETO.Script'
-import {Text} from "UnityEngine.UI";
-
-export default class ReportItems extends ZepetoScriptBehaviour {
-
-    public sessionIdText:Text;
-    public lapTimeText:Text;
-    
-}
